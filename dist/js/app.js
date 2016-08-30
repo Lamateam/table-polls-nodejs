7dfedf629fb7fbfe9c3d51abcb3ace21c3809287
Koroffin <n.koroffin@gmail.com>
2016-08-30 07:10:58 +0300

    first iteration

--- dist/js/app.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  define('app', ['angular'], function() {
-    return angular.module('app', []).config(function($interpolateProvider) {
-      return $interpolateProvider.startSymbol('[[').endSymbol(']]');
-    });
-  });
-
-}).call(this);
